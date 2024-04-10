b0c0a45fcbc0a259f87046788ad9fc13fe9d2352
real-varshney <109132583+real-varshney@users.noreply.github.com>
2024-04-10 18:09:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.get("/sendemail", async (req, res) => {
   if (!userEmail) {
      return res.status(400).json({ error: "Email is required" });
   }
-  const url = process.env.FRONTENDDOMAIN
-  const verificationLink = `${url}/verify?username=${username}&token=${token}`;
+
+  const verificationLink = `http://localhost:3000/verify?username=${username}&token=${token}`;
  
- const { data, error } = await sender.emails.send({
+  const { data, error } = await sender.emails.send({
    from: 'onboarding@resend.dev',
    to: userEmail, 
    subject: 'Welcome to dribbble! Please Verify Your Email',
@@ -38,15 +38,22 @@ app.get("/sendemail", async (req, res) => {
         <p>Hello ${username},</p>
         <p>Thank you for signing up! We're excited to have you on board. To complete your account setup, please verify your email address by clicking the button below:</p>
         <a href="${verificationLink}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block; margin-top: 20px;">Verify Email</a>
-        <p>If you didn't sign up for [Your Platform], you can ignore this email.</p>
-        <p>Thank you for choosing [Your Platform]!</p>
+        <p>If you didn't sign up for dribbble, you can ignore this email.</p>
+        <p>Thank you for choosing dribbble!</p>
         <p>Best Regards,</p>
-        <p>[Your Name]</p>
-        <p>[Your Position]</p>
-        <p>[Your Contact Information]</p>
+        <p>Vishal Varshney</p>
+        <p>FullStack developer</p>
+        <p>vishalvarshney888@gmail.com</p>
       </div>
    `,
   });
+ 
+  if (error) {
+     return res.status(400).json({ error });
+  }
+ 
+  res.status(200).json({ data });
+ });
  
 app.get('/verify', async(req, res) => {
    const username = req.query.username;
